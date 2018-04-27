c6cd65b74c7602c47b839a5de5d45296fc8cd8d5
Erick <churanoms@gmail.com>
2018-04-26 21:36:14 -0500

    design Modern

--- script/maple-message.js
+++ script/maple-message.js
@@ -1,3 +1,22 @@
 class MConfig{
+    static DISPLACE = true;
+    static WRITING = true;
+    
+}
 
+class MampleMessage{
+    constructor(el){
+        this.container = el;
+    }
+    html(){
+        //parent
+        let parentElement = document.createElement('div')
+        parentElement.setAttribute('class', 'm-msg')
+
+        //head
+
+    }
+    show(){
+
+    }
 }
